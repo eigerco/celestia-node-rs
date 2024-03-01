31db96a476a8e57241b39fb070d41de9e86f5dee
Maciej Zwoliński <mac.zwolinski@gmail.com>
2024-03-01 11:26:35 +0100

    integration test with shwap

--- cli/src/native.rs
+++ cli/src/native.rs
@@ -5,19 +5,15 @@ use std::time::Duration;
 use anyhow::{bail, Context, Result};
 use celestia_rpc::prelude::*;
 use celestia_rpc::Client;
+use celestia_types::nmt::Namespace;
 use clap::Parser;
-use directories::ProjectDirs;
 use libp2p::{identity, multiaddr::Protocol, Multiaddr};
-use lumina_node::blockstore::SledBlockstore;
+use lumina_node::blockstore::InMemoryBlockstore;
 use lumina_node::network::{canonical_network_bootnodes, network_genesis, network_id, Network};
 use lumina_node::node::{Node, NodeConfig};
-use lumina_node::store::{SledStore, Store};
-use sled::Db;
-use tokio::fs;
-use tokio::task::spawn_blocking;
+use lumina_node::store::{InMemoryStore, Store};
 use tokio::time::sleep;
 use tracing::info;
-use tracing::warn;
 
 use crate::common::ArgNetwork;
 
@@ -59,14 +55,8 @@ pub(crate) async fn run(args: Params) -> Result<()> {
     let genesis_hash = network_genesis(network);
 
     info!("Initializing store");
-    let db = open_db(args.store, &network_id).await?;
-    let store = SledStore::new(db.clone()).await?;
-    let blockstore = SledBlockstore::new(db).await?;
-
-    match store.head_height().await {
-        Ok(height) => info!("Initialised store with head height: {height}"),
-        Err(_) => info!("Initialised new store"),
-    }
+    let store = InMemoryStore::new();
+    let blockstore = InMemoryBlockstore::new();
 
     let node = Node::new(NodeConfig {
         network_id,
@@ -83,40 +73,23 @@ pub(crate) async fn run(args: Params) -> Result<()> {
     node.wait_connected_trusted().await?;
 
     // We have nothing else to do, but we want to keep main alive
-    loop {
-        sleep(Duration::from_secs(1)).await;
-    }
-}
 
-async fn open_db(path: Option<PathBuf>, network_id: &str) -> Result<Db> {
-    if let Some(path) = path {
-        let db = spawn_blocking(|| sled::open(path)).await??;
-        return Ok(db);
-    }
+    sleep(Duration::from_secs(15)).await;
 
-    let cache_dir =
-        ProjectDirs::from("co", "eiger", "lumina").context("Couldn't find lumina's cache dir")?;
-    let mut cache_dir = cache_dir.cache_dir().to_owned();
+    for row in 0..2 {
+        let row_block = node.request_row(row, 2).await.unwrap();
+        info!("Got row: {row_block:?}");
 
-    // TODO: remove it in 2 months or after a few releases
-    // If we find an old ('celestia') cache dir, move it to the new one.
-    if let Some(old_cache_dir) = ProjectDirs::from("co", "eiger", "celestia") {
-        let old_cache_dir = old_cache_dir.cache_dir();
-        if old_cache_dir.exists() && !cache_dir.exists() {
-            warn!(
-                "Migrating old cache dir to a new location: {} -> {}",
-                old_cache_dir.display(),
-                cache_dir.display()
-            );
-            fs::rename(old_cache_dir, &cache_dir).await?;
+        for ns in [Namespace::TRANSACTION /*, Namespace::PARITY_SHARE */] {
+            let nd_block = node.request_namespaced_data(ns, row, 2).await.unwrap();
+            info!("Got nd: {nd_block:?}");
         }
     }
 
-    cache_dir.push(network_id);
-    // TODO: should we create there also a subdirectory for the 'db'
-    // in case we want to put there some other stuff too?
-    let db = spawn_blocking(|| sled::open(cache_dir)).await??;
-    Ok(db)
+    let sampling = node.get_sampling_metadata(2).await.unwrap();
+    info!("Sampled block 2: {sampling:?}");
+
+    Ok(())
 }
 
 /// Get the address of the local bridge node
